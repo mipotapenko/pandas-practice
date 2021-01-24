a8a588ec61cb039865103dd815a66c9ff32b5fab
mipotapenko <mipotapenko@gmail.com>
2021-01-24 13:30:33 -0500

    Exercise 7 Complete

--- ex007/ex007.py
+++ ex007/ex007.py
@@ -11,4 +11,12 @@ ser2 = pd.Series([4, 5, 6, 7, 8])
 
 # Solution
 
-print(ser1.compare(ser2, align_axis=0))
+combined_series = pd.concat([ser1[~ser1.isin(ser2)], ser2[~ser2.isin(ser1)]], axis=0)
+
+# Website suggests using 1d intersection and 1d difference to form the final series.
+# The difference between my solution and the website is theirs preserves indices of the union...
+# while mine preserves the indices of the original list
+
+import helper_funcs as hf
+
+hf.write_results_str("007", [combined_series])
