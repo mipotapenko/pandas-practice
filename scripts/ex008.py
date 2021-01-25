bdf4695f79506e47508b841eb5cfde1466d8de2e
mipotapenko <mipotapenko@gmail.com>
2021-01-24 21:53:46 -0500

    Exercise 8 complete

---
+++ scripts/ex008.py
@@ -0,0 +1,18 @@
+# 8. How to get the minimum, 25th percentile, median, 75th, and max of a numeric series?
+
+import numpy as np
+import pandas as pd
+import helper_funcs as hf
+
+# Input
+ser = pd.Series(np.random.normal(10, 5, 25))
+
+# Solution
+summary_series = ser.describe()
+
+# The Website suggests
+# print(np.percentile(ser, q=[0, 25, 50, 75, 100]))
+# Also possible is
+# print(ser.quantile([0, 0.25, 0.5, 0.75, 1.0]))
+print(summary_series)
+hf.write_results_str("008", summary_series)
